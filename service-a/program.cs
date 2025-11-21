fd0be590b1800377e4546bd33577d1abe8cdbe1b
xentra-gabrielynigojavierto <gabriel.javierto@xentragroup.com>
2025-11-21 21:34:07 +0800

    test2

--- service-a/program.cs
+++ service-a/program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello DevOps TEAM! 21/11/2025");
+app.MapGet("/", () => "Hello DevOps TEAM! test 2 21/11/2025");
 
 app.Run();
